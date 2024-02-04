84634d4b4406da3acec6af49d75cf1266b22136f
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-02-04 14:20:49 -0500

    Added some comments on constants

--- RobotCode/src/main/java/frc/robot/constants/ShooterMountConstants.java
+++ RobotCode/src/main/java/frc/robot/constants/ShooterMountConstants.java
@@ -4,9 +4,8 @@ public class ShooterMountConstants
 {
 
 	public static final double DEGREES_IN_ONE_TICK = 360 / 42;
-	public static final double SPEED = 1;
-	public static final double MAXIMUM_ANGLE = 90;
-	public static final double MINIMUM_ANGLE = 20;
+	public static final double SPEED = 1; // Unused for now
+	public static final double MAXIMUM_ANGLE = 90, MINIMUM_ANGLE = 20; // Unused for now
 	public static final double DEADBAND = 0.5;
 
 	public static final double KP = 0.1;
