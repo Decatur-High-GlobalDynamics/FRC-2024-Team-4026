163d169e9fe4291d938aeccab5c8e89aa831a899
seankid <97747093+seankid@users.noreply.github.com>
2024-04-30 17:18:24 -0400

    AGHHHHHHHHHHHHHHH

--- RobotCode/src/main/java/frc/robot/commands/ZeroShooterMountCommand.java
+++ RobotCode/src/main/java/frc/robot/commands/ZeroShooterMountCommand.java
@@ -1,13 +1,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ShooterMountSubsystem;
+import frc.lib.modules.shootermount.ShooterMountSubsystem;
 
-public class ZeroShooterMountCommand extends Command 
+public class ZeroShooterMountCommand extends Command
 {
 
     ShooterMountSubsystem shooterMount;
-    
+
     public ZeroShooterMountCommand(ShooterMountSubsystem shooterMount)
     {
         this.shooterMount = shooterMount;
