ce4544db213d56f788487381186b73e315d06c17
Owen Beck <73498409+Beckoe@users.noreply.github.com>
2024-05-31 14:52:31 -0400

    Fixed errors in indexer

--- RobotCode/src/main/java/frc/lib/modules/indexer/IndexerSubsystem.java
+++ RobotCode/src/main/java/frc/lib/modules/indexer/IndexerSubsystem.java
@@ -1,12 +1,13 @@
 package frc.lib.modules.indexer;
 
+import com.revrobotics.CANSparkFlex;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.lib.core.motors.TeamSparkMAX;
 import frc.robot.constants.Constants;
 import frc.robot.constants.IndexerConstants;
 import frc.robot.constants.Ports;
@@ -18,7 +19,7 @@ public class IndexerSubsystem extends SubsystemBase
 	private double desiredIndexerVelocity;
 
 	private SparkPIDController indexerPid;
-	private TeamSparkMAX indexerMotorMain, indexerMotorSub;
+	private CANSparkFlex indexerMotorRight, indexerMotorLeft;
 	// beambreak is what we use to detect if there is a note
 	private DigitalInput beamBreak;
 
@@ -29,19 +30,19 @@ public class IndexerSubsystem extends SubsystemBase
 
 		beamBreak = new DigitalInput(Ports.BEAM_BREAK);
 
-		indexerMotorMain = new TeamSparkMAX("Left Shooter Motor Sub", Ports.INDEXER_MOTOR_MAIN);
-		indexerMotorSub = new TeamSparkMAX("Right Shooter Motor Sub", Ports.INDEXER_MOTOR_SUB);
+		indexerMotorRight = new CANSparkFlex(Ports.INDEXER_MOTOR_RIGHT, MotorType.kBrushless);
+		indexerMotorLeft = new CANSparkFlex(Ports.INDEXER_MOTOR_LEFT, MotorType.kBrushless);
 
-		indexerMotorSub.follow(indexerMotorMain, true);
+		indexerMotorLeft.follow(indexerMotorRight, true);
 
-		indexerMotorMain.enableVoltageCompensation(Constants.MAX_VOLTAGE);
-		indexerMotorSub.enableVoltageCompensation(Constants.MAX_VOLTAGE);
-		indexerMotorMain.setIdleMode(IdleMode.kBrake);
-		indexerMotorSub.setIdleMode(IdleMode.kBrake);
-		indexerMotorMain.setSmartCurrentLimit(Constants.MAX_CURRENT);
-		indexerMotorSub.setSmartCurrentLimit(Constants.MAX_CURRENT);
+		indexerMotorRight.enableVoltageCompensation(Constants.MAX_VOLTAGE);
+		indexerMotorLeft.enableVoltageCompensation(Constants.MAX_VOLTAGE);
+		indexerMotorRight.setIdleMode(IdleMode.kBrake);
+		indexerMotorLeft.setIdleMode(IdleMode.kBrake);
+		indexerMotorRight.setSmartCurrentLimit(Constants.NEO_MAX_CURRENT);
+		indexerMotorLeft.setSmartCurrentLimit(Constants.NEO_MAX_CURRENT);
 
-		indexerPid = indexerMotorMain.getPidController();
+		indexerPid = indexerMotorRight.getPIDController();
 
 		indexerPid.setP(IndexerConstants.INDEXER_KP);
 		indexerPid.setI(IndexerConstants.INDEXER_KI);
