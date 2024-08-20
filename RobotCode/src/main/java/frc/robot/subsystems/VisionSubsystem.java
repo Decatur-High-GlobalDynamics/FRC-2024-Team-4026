f90d65ee737809b047edeff02b3df71ebdf8ad34
sonofharri <82charkenn@csdecatur.net>
2024-08-20 16:30:47 -0400

    Owen I fixed your code ;)

--- RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ RobotCode/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,6 +4,7 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.modules.swervedrive.SwerveDriveSubsystem;
 import frc.robot.constants.Constants;
@@ -12,32 +13,31 @@ import frc.robot.constants.VisionConstants;
 public class VisionSubsystem extends SubsystemBase
 {
 
-	// private final PhotonCamera Camera;
-	// private PhotonPoseEstimator robotPoseEstimator;
+	private final PhotonCamera Camera;
+	private final PhotonPoseEstimator robotPoseEstimator;
 
 	private final SwerveDriveSubsystem Swerve;
 
 	public VisionSubsystem(SwerveDriveSubsystem swerve)
 	{
+		
 		Swerve = swerve;
 
-		// Camera = new PhotonCamera(VisionConstants.CAMERA_TABLE_NAME);
+		PhotonCamera Camera = new PhotonCamera(VisionConstants.CAMERA_TABLE_NAME);
 
-		// robotPoseEstimator = new PhotonPoseEstimator(Constants.AprilTagFieldLayout,
-		// PoseStrategy.LOWEST_AMBIGUITY, Camera,
-		// VisionConstants.ROBOT_TO_CAMERA_OFFSET);
+		 robotPoseEstimator = new PhotonPoseEstimator(Constants.AprilTagFieldLayout, PoseStrategy.LOWEST_AMBIGUITY, Camera, VisionConstants.ROBOT_TO_CAMERA_OFFSET);
 	}
 
 	public boolean noteObjectFound(boolean b)
 	{
-		// return Camera.hasTargets();
-		return false;
+		return Camera.hasTargets();
+	
 	}
 
 	@Override
 	public void periodic()
 	{
-		// Swerve.updatePoseWithVision(robotPoseEstimator.update());
+		Swerve.updatePoseWithVision(robotPoseEstimator.update());
 	}
 
 }
